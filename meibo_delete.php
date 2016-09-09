63d1fa900d8ac9d38288f2db8f6534582916b485
Zhengyu Yan <a06@julyinc.com>
2016-09-09 11:45:21 +0900

    とりあえずの削除ページ

---
+++ meibo_delete.php
@@ -0,0 +1,42 @@
+<?php
+require_once "sqlConn.php";
+$error = [];
+$stuID = htmlspecialchars($_GET["stuID"]);
+$name = rawurldecode(htmlspecialchars($_GET["name"]));
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+<div id="wrapper">
+
+    <?php
+    if (!empty($stuID) && !empty($name)) {
+        try {
+            $pdo->beginTransaction();
+            $sql1 = "DELETE FROM students WHERE stuID={$stuID}";
+            $sql2 = "DELETE FROM score WHERE stuID={$stuID}";
+
+            $del1 = $pdo->prepare($sql1);
+            $del2 = $pdo->prepare($sql2);
+            $del1->execute();
+            $del2->execute();
+            $pdo->commit();
+            echo "削除成功しました。<br>";
+            echo "<a href='meibo_show.php'>一覧に戻ります</a>";
+//    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            $pdo->rollBack();
+            echo $e->getMessage();
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
+
+
