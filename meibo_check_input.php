2236ad4e65c8c68ce17568b43b3043ea9712dfb1
C3\ebisuya <a02@julyinc.com>
2016-09-09 11:53:45 +0900

    登録画面＆チェック画面

---
+++ meibo_check_input.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>確認</title>
+</head>
+<body>
+<div>
+
+    <?php
+
+    $user='meibo';
+    $password = 'Aug.2016';
+    $dbName = 'meibo';
+    $host = 'localhost:3306';
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+    $name=$_POST['name'];
+    $age=$_POST['age'];
+    $gender=$_POST['gender'];
+    $enterYM=$_POST['enterYM'];
+    $sotsuYM=$_POST['sotsuYM'];
+
+    htmlspecialchars($name);
+
+
+    try{
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "データベースに接続したた";
+
+    }catch(Exception $e){
+        $err = "<span class='error'>エラーがありました</span>";
+        $err .= $e->getMessage();
+        exit($err);
+    }
+
+
+    //エラーを入れる配列
+    $errors =[];
+
+    if (!isset($name) || ($name==="")){
+        $errors[]="氏名を入力してください。";
+    }
+    if (!isset($age) || ($age==="")) {
+        $errors[] = "年齢を入力してください。";
+    }    elseif(!ctype_digit($age)){
+        $errors[]="年齢に整数を入力してください。";
+    }
+    if (!isset($enterYM) || ($enterYM==="")){
+        $errors[]="入学年月を入力してください。";
+    }
+    if (!isset($sotsuYM) || ($sotsuYM==="")){
+        $errors[]="卒業年月を入力してください。";
+    }
+
+    //エラーがあった時確認画面
+    if (count($errors)>0) {
+        echo '<ol>';
+        foreach ($errors as $value) {
+            echo "<li>", $value, "</li>";
+        }
+        echo "</ol>";
+        echo "<hr>";
+        echo "<a href='meibo_input.php'>戻る</a>";
+        // exit();
+    }
+//正常時登録
+    else{
+
+
+        //sql文を作る（新規レコード追加）
+        $sql ="INSERT students (name, age, gender, enterYM, sotsuYM) VALUES
+    ('$name','$age','$gender','enterYM','sotsuYM')";
+        //プリペアドステートメントを作る
+        $stm=$pdo->prepare($sql);
+        //SQL文を実行する
+        $stm->execute();
+
+
+        echo "<ul style='list-style:none;'>";
+        echo "<li>氏名：". $name ."</li><br>";
+        echo "<li>年齢：". $age ."</li><br>";
+        echo "<li>性別：". $gender ."</li><br>";
+        echo "<li>入学年月：". $enterYM ."</li><br>";
+        echo "<li>卒業年月：". $sotsuYM ."</li><br>";
+        echo "</ul>";
+        echo "<hr>";
+        echo "この内容で登録しました。<br>";
+    }
+
+    ?>
+
+
+</div>
+</body>
+</html>
---
+++ meibo_input.php
@@ -0,0 +1,51 @@
+<?php
+
+$user='meibo';
+$password = 'Aug.2016';
+$dbName = 'meibo';
+$host = 'localhost:3306';
+$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+try{
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "データベースに接続したた";
+
+}catch(Exception $e){
+    $err = "<span class='error'>エラーがありました</span>";
+    $err .= $e->getMessage();
+    exit($err);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>登録フォーム</title>
+</head>
+<body>
+<div>
+    <p>登録する内容を入力してください。</p>
+    <form method="post" action="meibo_check_input.php">
+
+        生徒名：
+        <input type="text" name="name" placeholder="名前を入れてください。"><br>
+        年齢：
+        <input type="number" name="age" placeholder="半角数字"><br>
+        性別：
+        <select name="gender">
+            <option value="男">男性</option>
+            <option value="女">女性</option>
+        </select><br>
+        入学年月：
+        <input type="date" name="enterYM"><br>
+        卒業年月：
+        <input type="date" name="sotsuYM"><br>
+        <input type="submit" value="登録"><br>
+
+    </form>
+</div>
+</body>
+</html>
